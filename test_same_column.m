216bf7342af12cf7b1ca2dedb32dd8f1dc3a5b3f
doublnt <yinximail@gmail.com>
2018-04-09 12:06:09 +0800

    Update code

--- test_same_column.m
+++ test_same_column.m
@@ -1,11 +1,12 @@
-A = [ 1 2 3; 4 5 6; 1 2 3;1 1 1];
+A = [ 1 1 1 0; 1 1 1 0; 1 0 0 1;1 0 1 1];
 sizeA = size(A);
-B = zeros(1,sizeA(1,2));
-
+B = zeros(sizeA(1,1),sizeA(1,2));
+C = zeros(1,sizeA(1,2)-1);
 
 for i =1 :sizeA(1,1)-1
-   temp = same_column_demo(A(i,:),A(sizeA(1,1),:));
-   B = B + temp;
+   temp = same_column_demo(A(:,i),A(:,sizeA(1,2)));
+   B(:,i) = B(:,i) + temp;
+   C(1,i) = sum(B(:,i)); 
 end
 
-B
+C
